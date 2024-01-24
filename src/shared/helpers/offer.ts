ccc90edc62548e0241c18c1735e1bb3812266f49
xchaikax <artem.akhaltsev@gmail.com>
2024-01-24 20:58:03 +0100

    [module2-task1] adds helper functions

---
+++ src/shared/helpers/offer.ts
@@ -0,0 +1,53 @@
+import { City, Facility, Offer, PropertyType, User, UserType } from "../types/index.js";
+
+export function createOffer(data: string): Offer {
+  const [
+    createdDate,
+    title,
+    description,
+    city,
+    previewImage,
+    images,
+    isPremium,
+    isFavorite,
+    rating,
+    propertyType,
+    roomsNumber,
+    guestsNumber,
+    price,
+    facilities,
+    user,
+    commentsCount,
+    coordinates,
+  ] = data.replace("\n", "").split("\t");
+
+  return {
+    createdDate: new Date(createdDate),
+    title,
+    description,
+    city: City[city as keyof typeof City],
+    previewImage,
+    images: images.split(";"),
+    isPremium: JSON.parse(isPremium),
+    isFavorite: JSON.parse(isFavorite),
+    rating: Number.parseFloat(rating),
+    propertyType: propertyType as PropertyType,
+    roomsNumber: Number.parseInt(roomsNumber, 10),
+    guestsNumber: Number.parseInt(guestsNumber, 10),
+    price: Number.parseInt(price, 10),
+    facilities: facilities.split(";").map((facility) => facility as Facility),
+    user: parseUser(user),
+    commentsCount: Number.parseInt(commentsCount, 10),
+    coordinates: parseCoordinates(coordinates),
+  };
+}
+
+function parseUser(user: string): User {
+  const [name, email, avatar, password, userType] = user.split(";");
+  return { name, email, avatar, password, userType: userType as UserType };
+}
+
+function parseCoordinates(location: string): { latitude: number, longitude: number } {
+  const [latitude, longitude] = location.split(";");
+  return { latitude: Number.parseFloat(latitude), longitude: Number.parseFloat(longitude) };
+}
