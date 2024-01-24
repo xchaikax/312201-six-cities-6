87cc599f3a0ddcebf88454ec86266c81d96804d1
xchaikax <artem.akhaltsev@gmail.com>
2024-01-24 20:55:36 +0100

    [module2-task1] changes TSVFileReader to use readStreams

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,73 +1,38 @@
-import { readFileSync } from "node:fs";
+import { createReadStream } from "node:fs";
+import { EventEmitter } from "node:events";
 import { FileReader } from "./file-reader.interface.js";
-import { City, Facility, Offer, PropertyType, User, UserType } from "../../types/index.js";
 
-export class TSVFileReader implements FileReader {
-  private rawData = "";
+const CHUNK_SIZE = 16384; // 16KB
 
+export class TSVFileReader extends EventEmitter implements FileReader {
   constructor(
     private readonly filename: string,
-  ) {}
-
-  public read(): void {
-    this.rawData = readFileSync(this.filename, { encoding: "utf-8" });
+  ) {
+    super();
   }
 
-  public toArray(): Offer[] {
-    if (!this.rawData) {
-      throw new Error("File was not read");
+  public async read(): Promise<void> {
+    const readStream = createReadStream(this.filename, {
+      highWaterMark: CHUNK_SIZE,
+      encoding: "utf-8",
+    });
+
+    let remainingData = "";
+    let nextLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of readStream) {
+      remainingData += chunk.toString();
+
+      while ((nextLinePosition = remainingData.indexOf("\n")) >= 0) {
+        const completeRow = remainingData.slice(0, nextLinePosition + 1);
+        remainingData = remainingData.slice(++nextLinePosition);
+        importedRowCount++;
+
+        this.emit("line", completeRow);
+      }
     }
 
-    return this.rawData
-      .split("\n")
-      .filter((row) => row.trim().length > 0)
-      .map((line) => line.split("\t"))
-      .map(([
-        title,
-        description,
-        postDate,
-        city,
-        previewImage,
-        propertyImages,
-        isPremium,
-        isFavorite,
-        rating,
-        propertyType,
-        roomsNumber,
-        guestsNumber,
-        price,
-        facilities,
-        user,
-        commentsCount,
-        location,
-      ]) => ({
-        title,
-        description,
-        postDate: new Date(postDate),
-        city: City[city as keyof typeof City],
-        previewImage,
-        propertyImages: propertyImages.split(";"),
-        isPremium: JSON.parse(isPremium),
-        isFavorite: JSON.parse(isFavorite),
-        rating: Number.parseFloat(rating),
-        propertyType: propertyType as PropertyType,
-        roomsNumber: Number.parseInt(roomsNumber, 10),
-        guestsNumber: Number.parseInt(guestsNumber, 10),
-        price: Number.parseInt(price, 10),
-        facilities: facilities.split(";").filter(Boolean).map((facility) => facility as Facility),
-        user: this.parseUser(user),
-        commentsCount: Number.parseInt(commentsCount, 10),
-        coordinates: this.parseCoordinates(location),
-      }));
-  }
-
-  private parseUser(user: string): User {
-    const [name, email, avatar, password, userType] = user.split(";");
-    return { name, email, avatar, password, userType: UserType[userType as keyof typeof UserType] };
-  }
-
-  private parseCoordinates(location: string): { latitude: number, longitude: number } {
-    const [latitude, longitude] = location.split(";");
-    return { latitude: Number.parseFloat(latitude), longitude: Number.parseFloat(longitude) };
+    this.emit("end", importedRowCount);
   }
 }
