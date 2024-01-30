d231f8a419ffd8a8ccb4cb70be3ed895910ab820
xchaikax <artem.akhaltsev@gmail.com>
2024-01-30 22:03:04 +0100

    [module3-task1] adds logger

---
+++ src/shared/libs/logger/pino.logger.ts
@@ -0,0 +1,48 @@
+import { resolve } from "node:path";
+import { Logger as PinoInstance, pino, transport } from "pino";
+import { Logger } from "./logger.interface.js";
+import { getCurrentModuleDirectoryPath } from "../../helpers/index.js";
+
+class PinoLogger implements Logger {
+  private readonly logger: PinoInstance;
+
+  constructor() {
+    const modulePath = getCurrentModuleDirectoryPath();
+    const logFilePath = "logs/rest.log";
+    const destination = resolve(modulePath, "../../../", logFilePath);
+
+    const multiTransport = transport({
+      targets: [
+        {
+          target: "pino/file",
+          options: { destination },
+          level: "debug",
+        },
+        {
+          target: "pino/file",
+          options: {},
+          level: "info",
+        },
+      ],
+    });
+
+    this.logger = pino({}, multiTransport);
+    this.logger.info("Logger created…");
+  }
+
+  public debug(message: string, ...args: unknown[]): void {
+    this.logger.debug(message, ...args);
+  }
+
+  public error(message: string, error: Error, ...args: unknown[]): void {
+    this.logger.error(error, message, ...args);
+  }
+
+  public warn(message: string, ...args: unknown[]): void {
+    this.logger.warn(message, ...args);
+  }
+
+  public info(message: string, ...args: unknown[]): void {
+    this.logger.info(message, ...args);
+  }
+}
