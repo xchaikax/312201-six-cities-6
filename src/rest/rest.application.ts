95d4b0d750f9ab0901e0a98b8d559a9980196938
xchaikax <artem.akhaltsev@gmail.com>
2024-01-30 22:37:41 +0100

    [module3-task1] adds rest application

---
+++ src/rest/rest.application.ts
@@ -0,0 +1,14 @@
+import { Logger } from "../shared/libs/logger/index.js";
+import { Config, RestSchema } from "../shared/libs/config/index.js";
+
+export class RestApplication {
+  constructor(
+    private readonly logger: Logger,
+    private readonly config: Config<RestSchema>,
+  ) {}
+
+  public async init() {
+    this.logger.info("Application initialization");
+    this.logger.info(`Get value from env $PORT: ${this.config.get("PORT")}`);
+  }
+}
