63cdc33279aaf1b159e29c861bb07c6b43302102
xchaikax <artem.akhaltsev@gmail.com>
2024-02-23 15:17:00 +0100

    [module6-task1] removes unnecessary stuff

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -4,10 +4,8 @@ import { RestApplication } from "./rest/index.js";
 import { Component } from "./shared/types/index.js";
 import { createUserContainer } from "./shared/modules/user/index.js";
 import { createRestApplicationContainer } from "./rest/rest.container.js";
-import { createOfferContainer, OfferService } from "./shared/modules/offer/index.js";
-import { CommentService, createCommentContainer } from "./shared/modules/comment/index.js";
-// import { Logger } from "./shared/libs/logger/index.js";
-import { generateRandomValue } from "./shared/helpers/index.js";
+import { createOfferContainer } from "./shared/modules/offer/index.js";
+import { createCommentContainer } from "./shared/modules/comment/index.js";
 
 async function bootstrap() {
   const appContainer = Container.merge(
@@ -19,22 +17,6 @@ async function bootstrap() {
 
   const application = appContainer.get<RestApplication>(Component.RestApplication);
   await application.init();
-
-  // const logger = appContainer.get<Logger>(Component.Logger);
-
-  const os = appContainer.get<OfferService>(Component.OfferService);
-  const cs = appContainer.get<CommentService>(Component.CommentService);
-
-  await cs.create({
-    authorId: "65d2636b5524807cef831d0b",
-    offerId: "65d2636b5524807cef831d0d",
-    text: "Hello, world!",
-    rating: generateRandomValue(1, 5),
-  });
-
-  await os.findById("65d2636b5524807cef831d59");
-
-  // logger.info(data as never);
 }
 
 bootstrap();
