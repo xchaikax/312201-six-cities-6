ceb852bb62bfb2c308edcae21fd4bd558b4e8e66
xchaikax <artem.akhaltsev@gmail.com>
2024-02-25 00:35:11 +0100

    [module6-task1] updates offer container

--- src/shared/modules/offer/offer.container.ts
+++ src/shared/modules/offer/offer.container.ts
@@ -4,12 +4,14 @@ import { Component } from "../../types/index.js";
 import { OfferService } from "./offer-service.interface.js";
 import { BaseOfferService } from "./offer.service.js";
 import { OfferEntity, OfferModel } from "./offer.entity.js";
+import { OfferController } from "./offer.controller.js";
 
 export function createOfferContainer() {
   const offerContainer = new Container();
 
   offerContainer.bind<OfferService>(Component.OfferService).to(BaseOfferService).inSingletonScope();
   offerContainer.bind<types.ModelType<OfferEntity>>(Component.OfferModel).toConstantValue(OfferModel);
+  offerContainer.bind<OfferController>(Component.OfferController).to(OfferController).inSingletonScope();
 
   return offerContainer;
 }
